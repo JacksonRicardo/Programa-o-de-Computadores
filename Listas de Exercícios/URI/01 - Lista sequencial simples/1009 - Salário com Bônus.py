5884433953873b52428ef384c3ca2ddd2c57be12
JacksonRicardo <42697964+JacksonRicardo@users.noreply.github.com>
2022-07-21 15:26:29 -0300

    Adicionando mais questões
    Todas as questões em Python

--- Listas de Exercícios/URI/01 - Lista sequencial simples/1009 - Salário com Bônus.py
+++ Listas de Exercícios/URI/01 - Lista sequencial simples/1009 - Salário com Bônus.py
@@ -1,4 +1,4 @@
-a, b, c = input(), float(input()), float(input())
-reaj = c * 0.15
-new = b + reaj
-print('TOTAL = R$ %.2f' %new)
+nome, salario_fixo, total_vendas = input(), float(input()), float(input())
+real = total_vendas * 0.15
+novo = salario_fixo + real
+print('TOTAL = R$ %.2f' %novo)
